8f08c835b82c5e7743ae6ee790e3ae16746a9812
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2017-12-12 00:35:00 -0200

    Its just working!!

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Yang Ricardo  on 10/12/2017.
+//  Copyright © 2017 Yang Ricardo . All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    //MARK: Properties
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
