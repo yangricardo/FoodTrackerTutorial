c02335d971b31c59e539921c88422a811192cefc
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2017-12-12 01:32:15 -0200

    Tabs bar

---
+++ FoodTracker/MealCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  MealCollectionViewCell.swift
+//  FoodTracker
+//
+//  Created by Yang Ricardo  on 12/12/2017.
+//  Copyright © 2017 Yang Ricardo . All rights reserved.
+//
+
+import UIKit
+
+class MealCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    
+}
